0cb36bf40ae36026a9e8e2bda6bacd7fcdf6f1fb
waidesoper <waide.soper@gmail.com>
2024-01-10 16:16:39 +1100

    Updated to 1.20.2

--- src/main/java/crimsonfluff/elytranofallfabric/ElytranofallFabric.java
+++ src/main/java/crimsonfluff/elytranofallfabric/ElytranofallFabric.java
@@ -9,22 +9,24 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ElytranofallFabric implements ModInitializer {
     public static final String MOD_ID = "elytranofall";
     public static ElytraNoFallConfig CONFIG;
-
+    public static final Logger LOGGER = LogManager.getLogger();
     @Override
     public void onInitialize() {
         AutoConfig.register(ElytraNoFallConfig.class, GsonConfigSerializer::new);
         CONFIG = AutoConfig.getConfigHolder(ElytraNoFallConfig.class).getConfig();
 
-        ConfigBuilder builder = ConfigBuilder.create().setTitle(new TranslatableText("title.elytranofall.config"));
+        ConfigBuilder builder = ConfigBuilder.create().setTitle(Text.of("Elytra No Fall Config"));
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
         onLivingDamagedCallback.EVENT.register((entity, source) ->{
@@ -32,8 +34,9 @@ public class ElytranofallFabric implements ModInitializer {
 
             ServerPlayerEntity player = (ServerPlayerEntity) entity;
             if(player.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof ElytraItem){
-                if(source == DamageSource.FLY_INTO_WALL || source == DamageSource.FALL){
+                if(source.getTypeRegistryEntry().matchesKey(DamageTypes.FLY_INTO_WALL)|| source.getTypeRegistryEntry().matchesKey(DamageTypes.FALL)){
                     if(EnchantmentHelper.getLevel(Enchantments.FEATHER_FALLING, player.getEquippedStack(EquipmentSlot.FEET)) >= CONFIG.elytraNoFall.general.requiresFeatherFalling){
+                        LOGGER.info("Damage avoided");
                         return ActionResult.FAIL;
                     }
                 }
